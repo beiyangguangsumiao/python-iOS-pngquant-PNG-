01583203ee87028fc900501e04eac4ceb0af14bc
beiyangguangsumiao <15662170832@163.com>
2018-11-23 16:47:09 +0800

    Python 用pngquant 压缩PNG使 APP瘦身10M以上
    根据官网https://pngquant.org/介绍，pngquant是国外的一个有损的PNG压缩开源库，提供了命令行形式和源码库形式。将24位或32位的RGBA PNG图转换成8位PNG图并保留透明度通道。通过这个库的转化可以显著减少png文件大小（通常减少70%）。生成的图片文件可以兼容所有现代web浏览器，在IE6下比24-bit PNGs也有更好的表现，具有快速模式，用于处理大批图片。源码库地址为pngquant。
    
    本地运行脚本，主要功能包括：
    
       1.会压缩根目录下的png 的图片；
    
       2.本地记录一份压缩过的所有图片信息；
    
       3.如果是相同名字的图片替换过，有三种情况：
    
            1两种图片大小相同，不会操作；
    
            2.新的图片小于旧的图片，不会压缩，更新图片信息;
    
            3.新的图片大于旧的图片，会压缩，更新图片信息；
    
         4.如果是新增，直接压缩，更新存储记录；
    
         5.用了这个脚本，项目较少11M。

---
+++ PngquantPicture.py
@@ -0,0 +1,97 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+# -*- author: lianxue.aho -*-
+
+import os
+import os.path
+import pickle
+
+
+def pngquantPicture(file):
+    
+    os.system("pngquant -f --ext .png --quality 50-80 \"" + file + "\"")
+    filesize = os.path.getsize(file)
+    dict = {file: filesize}
+    print dict
+    return dict;
+
+
+def dumpStorageImageInfo(imageInfoList):
+    
+    output = open('ProjectTools/storageImageInfo.pkl','wb')
+    pickle.dump(imageInfoList, output)
+    print imageInfoList
+    output.close()
+
+
+def loadStorageImageInfo():
+    
+    if os.path.isfile('ProjectTools/storageImageInfo.pkl'):
+       pkl_file = open('ProjectTools/storageImageInfo.pkl', 'rb')
+       imageInfoListStorage = pickle.load(pkl_file)
+#       print imageInfoListStorage
+       pkl_file.close()
+       return imageInfoListStorage
+
+
+def compareFileSize(imageInfoListStorage, file):
+    
+    oldImage = 0
+    for index, imageInfoDict in enumerate(imageInfoListStorage):
+        if file in imageInfoDict:
+           oldImage = 1
+           fileSizeOld = imageInfoDict[file]
+           fileSizeNew = os.path.getsize(file)
+           if fileSizeOld == fileSizeNew:
+#              print 'image size equal to'
+              return
+           elif fileSizeOld < fileSizeNew:
+              dict = pngquantPicture(file)
+              imageInfoListStorage[index] = dict
+              dumpStorageImageInfo(imageInfoListStorage)
+#              print 'pngquant picture update storage'
+              return
+           elif fileSizeOld > fileSizeNew:
+              imageInfoListStorage[index] = {file: fileSizeNew}
+              dumpStorageImageInfo(imageInfoListStorage)
+#              print 'update fileSize update storage'
+
+    return oldImage
+
+
+
+def compress(path):
+    
+    imageInfoList = []
+
+    imageInfoListStorage = loadStorageImageInfo();
+
+    for dir_path, dir_names, file_names in os.walk(path):
+        file_names = filter(lambda file_name: file_name[-4:] == '.png', file_names)
+        file_names = map(lambda file_name: os.path.join(dir_path, file_name), file_names)
+        
+        for file in file_names:
+            print file
+            if imageInfoListStorage:
+               oldImage = compareFileSize(imageInfoListStorage, file)
+#               print 'image are stored, not have to add picture'
+               if oldImage == 0:
+                  dict = pngquantPicture(file)
+                  imageInfoListStorage.append(dict)
+#                  print 'image are stored, have to add picture'
+            else:
+               dict = pngquantPicture(file)
+               imageInfoList.append(dict)
+#               print 'no image stored, have to add picure'
+
+                
+    if imageInfoListStorage:
+       dumpStorageImageInfo(imageInfoListStorage)
+#       print 'update storage picture'
+    elif imageInfoList:
+       dumpStorageImageInfo(imageInfoList)
+#       print 'add storage picture'
+
+if __name__ == '__main__':
+    compress(os.getcwd())
